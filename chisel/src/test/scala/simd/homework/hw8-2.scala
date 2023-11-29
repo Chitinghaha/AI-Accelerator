af4c140efa4780caa5d1063279f9e4b022d19b9a
Jen-Chien Chang <nn6124014@gs.ncku.edu.tw>
2023-11-29 13:01:50 +0800

    Rename class names of homework testbench and remove testbench for INT8 to INT8 multiplications.

--- chisel/src/test/scala/simd/homework/hw8-2.scala
+++ chisel/src/test/scala/simd/homework/hw8-2.scala
@@ -7,7 +7,7 @@ import org.scalatest.freespec.AnyFreeSpec
 import chisel3.experimental.BundleLiterals._
 import chiseltest.{WriteVcdAnnotation, VerilatorBackendAnnotation}
 
-class TestLab2Addition extends AnyFreeSpec with ChiselScalatestTester {
+class TestHw2Addition extends AnyFreeSpec with ChiselScalatestTester {
   "SIMD Execution Unit should execute sADDI8S.vv instructions" in {
     test(new simd.SIMDEngine()).withAnnotations(Seq(
       WriteVcdAnnotation,
@@ -141,7 +141,7 @@ class TestLab2Addition extends AnyFreeSpec with ChiselScalatestTester {
   }
 }
 
-class TestLab2Substraction extends AnyFreeSpec with ChiselScalatestTester {
+class TestHw2Substraction extends AnyFreeSpec with ChiselScalatestTester {
   "SIMD Execution Unit should execute sSUBI8S.vv instructions" in {
     test(new simd.SIMDEngine()).withAnnotations(Seq(
       WriteVcdAnnotation,
@@ -275,40 +275,7 @@ class TestLab2Substraction extends AnyFreeSpec with ChiselScalatestTester {
   }
 }
 
-class TestLab2Multiplication extends AnyFreeSpec with ChiselScalatestTester {
-  "SIMD Execution Unit should execute sMULI8I8S.vv instructions" in {
-    test(new simd.SIMDEngine()).withAnnotations(Seq(
-      WriteVcdAnnotation,
-      VerilatorBackendAnnotation
-    )) { dut =>
-      val funct7 = Integer.parseInt("0000010", 2)
-      val funct3 = Integer.parseInt("000", 2)
-
-      for (i <- 0 until 50) {
-        val rs1_arr = (0 to 3).map(_ => Random.nextInt().toByte)
-        val rs2_arr = (0 to 3).map(_ => Random.nextInt().toByte)
-        val rd_arr  = (0 to 3).map(i => (rs1_arr(i) * rs2_arr(i)).toShort)
-
-        dut.io.cmd_payload.valid.poke(true.B)
-        dut.io.cmd_payload.bits.funct7.poke(funct7)
-        dut.io.cmd_payload.bits.funct3.poke(funct3)
-        dut.io.cmd_payload.bits.rs1.poke(
-          Seq.range(3, -1, -1).map { i => (rs1_arr(i).toInt & 0xff) << (8 * i) }.reduce(_ | _)
-        )
-        dut.io.cmd_payload.bits.rs2.poke(
-          Seq.range(3, -1, -1).map { i => (rs2_arr(i).toInt & 0xff) << (8 * i) }.reduce(_ | _)
-        )
-        dut.io.cmd_payload.ready.expect(true.B)
-        dut.io.rsp_payload.ready.poke(true.B)
-        dut.io.rsp_payload.valid.expect(true.B)
-        dut.io.rsp_payload.bits.rd.expect(
-          Seq.range(3, -1, -1).map { i => ((rd_arr(i).toInt & 0xff00) >> 8) << (8 * i) }.reduce(_ | _)
-        )
-        dut.clock.step(1)
-      }
-    }
-  }
-
+class TestHw2Multiplication extends AnyFreeSpec with ChiselScalatestTester {
   "SIMD Execution Unit should execute sMULI8I16S.vv.L instructions" in {
     test(new simd.SIMDEngine()).withAnnotations(Seq(
       WriteVcdAnnotation,
@@ -370,39 +337,6 @@ class TestLab2Multiplication extends AnyFreeSpec with ChiselScalatestTester {
         dut.io.rsp_payload.bits.rd.expect(Seq.range(1, -1, -1).map { i =>
           (rd_arr(i).toInt & 0xffff) << (16 * i)
         }.reduce(_ | _))
-        dut.clock.step(1)
-      }
-    }
-  }
-
-  "SIMD Execution Unit should execute sMULI8I8S.vx instructions" in {
-    test(new simd.SIMDEngine()).withAnnotations(Seq(
-      WriteVcdAnnotation,
-      VerilatorBackendAnnotation
-    )) { dut =>
-      val funct7 = Integer.parseInt("1000010", 2)
-      val funct3 = Integer.parseInt("000", 2)
-
-      for (i <- 0 until 50) {
-        val rs1_arr = (0 to 3).map(_ => Random.nextInt().toByte)
-        val rs2_arr = (0 to 3).map(_ => Random.nextInt().toByte)
-        val rd_arr  = (0 to 3).map(i => (rs1_arr(i) * rs2_arr(0)).toShort)
-
-        dut.io.cmd_payload.valid.poke(true.B)
-        dut.io.cmd_payload.bits.funct7.poke(funct7)
-        dut.io.cmd_payload.bits.funct3.poke(funct3)
-        dut.io.cmd_payload.bits.rs1.poke(
-          Seq.range(3, -1, -1).map { i => (rs1_arr(i).toInt & 0xff) << (8 * i) }.reduce(_ | _)
-        )
-        dut.io.cmd_payload.bits.rs2.poke(
-          Seq.range(3, -1, -1).map { i => (rs2_arr(i).toInt & 0xff) << (8 * i) }.reduce(_ | _)
-        )
-        dut.io.cmd_payload.ready.expect(true.B)
-        dut.io.rsp_payload.ready.poke(true.B)
-        dut.io.rsp_payload.valid.expect(true.B)
-        dut.io.rsp_payload.bits.rd.expect(
-          Seq.range(3, -1, -1).map { i => ((rd_arr(i).toInt & 0xff00) >> 8) << (8 * i) }.reduce(_ | _)
-        )
         dut.clock.step(1)
       }
     }
