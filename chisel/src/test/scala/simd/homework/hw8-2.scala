7997f0df47d9ff026e4243f309f609d57e800ff0
Jen-Chien Chang <nn6124014@gs.ncku.edu.tw>
2023-11-29 13:02:10 +0800

    Add testbench for quantization related instructions in HW8-2.

--- chisel/src/test/scala/simd/homework/hw8-2.scala
+++ chisel/src/test/scala/simd/homework/hw8-2.scala
@@ -408,3 +408,159 @@ class TestHw2Multiplication extends AnyFreeSpec with ChiselScalatestTester {
     }
   }
 }
+
+class TestHw2Quantization extends AnyFreeSpec with ChiselScalatestTester {
+  def quantize(value: Short, scaling: Int, zero: Int): Byte = {
+    var result: Int = 0
+    if (scaling >= 0) {
+      result = (value.toInt << scaling) + zero
+    } else {
+      result = (value.toInt >> (scaling * (-1))) + zero
+    }
+    return result.toByte
+  }
+
+  "SIMD Execution Unit should execute sMULI8I8S.vv instructions" in {
+    test(new simd.SIMDEngine()).withAnnotations(Seq(
+      WriteVcdAnnotation,
+      VerilatorBackendAnnotation
+    )) { dut =>
+      val qnt_info_funct7 = Integer.parseInt("0000111", 2)
+      val qnt_info_funct3 = Integer.parseInt("000", 2)
+      val funct7          = Integer.parseInt("0000010", 2)
+      val funct3          = Integer.parseInt("000", 2)
+
+      for (i <- 0 until 50) {
+        // setup quantization
+        val scaling_factor: Int = Random.nextInt(15) - 7
+        val zero: Int           = Random.nextInt(15) - 7
+
+        dut.io.cmd_payload.valid.poke(true.B)
+        dut.io.cmd_payload.bits.funct7.poke(qnt_info_funct7)
+        dut.io.cmd_payload.bits.funct3.poke(qnt_info_funct3)
+        dut.io.cmd_payload.bits.rs1.poke(scaling_factor)
+        dut.io.cmd_payload.bits.rs2.poke(zero)
+        dut.io.cmd_payload.ready.expect(true.B)
+        dut.clock.step(1)
+
+        // calculation
+        val rs1_arr = (0 to 3).map(_ => Random.nextInt().toByte)
+        val rs2_arr = (0 to 3).map(_ => Random.nextInt().toByte)
+        val rd_arr  = (0 to 3).map(i => quantize((rs1_arr(i) * rs2_arr(i)).toShort, scaling_factor, zero))
+
+        dut.io.cmd_payload.valid.poke(true.B)
+        dut.io.cmd_payload.bits.funct7.poke(funct7)
+        dut.io.cmd_payload.bits.funct3.poke(funct3)
+        dut.io.cmd_payload.bits.rs1.poke(
+          Seq.range(3, -1, -1).map { i => (rs1_arr(i).toInt & 0xff) << (8 * i) }.reduce(_ | _)
+        )
+        dut.io.cmd_payload.bits.rs2.poke(
+          Seq.range(3, -1, -1).map { i => (rs2_arr(i).toInt & 0xff) << (8 * i) }.reduce(_ | _)
+        )
+        dut.io.cmd_payload.ready.expect(true.B)
+        dut.io.rsp_payload.ready.poke(true.B)
+        dut.io.rsp_payload.valid.expect(true.B)
+        dut.io.rsp_payload.bits.rd.expect(
+          Seq.range(3, -1, -1).map { i => (rd_arr(i).toInt & 0xff) << (8 * i) }.reduce(_ | _)
+        )
+        dut.clock.step(1)
+      }
+    }
+  }
+
+  "SIMD Execution Unit should execute sMULI8I8S.vx instructions" in {
+    test(new simd.SIMDEngine()).withAnnotations(Seq(
+      WriteVcdAnnotation,
+      VerilatorBackendAnnotation
+    )) { dut =>
+      val qnt_info_funct7 = Integer.parseInt("0000111", 2)
+      val qnt_info_funct3 = Integer.parseInt("000", 2)
+      val funct7          = Integer.parseInt("1000010", 2)
+      val funct3          = Integer.parseInt("000", 2)
+
+      for (i <- 0 until 50) {
+        // setup quantization
+        val scaling_factor: Int = Random.nextInt(15) - 7
+        val zero: Int           = Random.nextInt(15) - 7
+
+        dut.io.cmd_payload.valid.poke(true.B)
+        dut.io.cmd_payload.bits.funct7.poke(qnt_info_funct7)
+        dut.io.cmd_payload.bits.funct3.poke(qnt_info_funct3)
+        dut.io.cmd_payload.bits.rs1.poke(scaling_factor)
+        dut.io.cmd_payload.bits.rs2.poke(zero)
+        dut.io.cmd_payload.ready.expect(true.B)
+        dut.clock.step(1)
+
+        // calculation
+        val rs1_arr = (0 to 3).map(_ => Random.nextInt().toByte)
+        val rs2_arr = (0 to 3).map(_ => Random.nextInt().toByte)
+        val rd_arr  = (0 to 3).map(i => quantize((rs1_arr(i) * rs2_arr(0)).toShort, scaling_factor, zero))
+
+        dut.io.cmd_payload.valid.poke(true.B)
+        dut.io.cmd_payload.bits.funct7.poke(funct7)
+        dut.io.cmd_payload.bits.funct3.poke(funct3)
+        dut.io.cmd_payload.bits.rs1.poke(
+          Seq.range(3, -1, -1).map { i => (rs1_arr(i).toInt & 0xff) << (8 * i) }.reduce(_ | _)
+        )
+        dut.io.cmd_payload.bits.rs2.poke(
+          Seq.range(3, -1, -1).map { i => (rs2_arr(i).toInt & 0xff) << (8 * i) }.reduce(_ | _)
+        )
+        dut.io.cmd_payload.ready.expect(true.B)
+        dut.io.rsp_payload.ready.poke(true.B)
+        dut.io.rsp_payload.valid.expect(true.B)
+        dut.io.rsp_payload.bits.rd.expect(
+          Seq.range(3, -1, -1).map { i => (rd_arr(i).toInt & 0xff) << (8 * i) }.reduce(_ | _)
+        )
+        dut.clock.step(1)
+      }
+    }
+  }
+
+  "SIMD Execution Unit should execute sQNT.I.H2B instructions" in {
+    test(new simd.SIMDEngine()).withAnnotations(Seq(
+      WriteVcdAnnotation,
+      VerilatorBackendAnnotation
+    )) { dut =>
+      val qnt_info_funct7 = Integer.parseInt("0000111", 2)
+      val qnt_info_funct3 = Integer.parseInt("000", 2)
+      val funct7          = Integer.parseInt("0000111", 2)
+      val funct3          = Integer.parseInt("001", 2)
+
+      for (i <- 0 until 50) {
+        // setup quantization
+        val scaling_factor: Int = Random.nextInt(15) - 7
+        val zero: Int           = Random.nextInt(15) - 7
+
+        dut.io.cmd_payload.valid.poke(true.B)
+        dut.io.cmd_payload.bits.funct7.poke(qnt_info_funct7)
+        dut.io.cmd_payload.bits.funct3.poke(qnt_info_funct3)
+        dut.io.cmd_payload.bits.rs1.poke(scaling_factor)
+        dut.io.cmd_payload.bits.rs2.poke(zero)
+        dut.io.cmd_payload.ready.expect(true.B)
+        dut.clock.step(1)
+
+        // calculation
+        val rs1_arr = (0 to 2).map(_ => Random.nextInt().toShort)
+        val rs2_arr = (0 to 2).map(_ => Random.nextInt().toShort)
+        val rd_arr  = (0 to 3).map(i => quantize((rs1_arr ++ rs2_arr)(i), scaling_factor, zero))
+
+        dut.io.cmd_payload.valid.poke(true.B)
+        dut.io.cmd_payload.bits.funct7.poke(funct7)
+        dut.io.cmd_payload.bits.funct3.poke(funct3)
+        dut.io.cmd_payload.bits.rs1.poke(
+          Seq.range(1, -1, -1).map { i => (rs1_arr(i).toInt & 0xff) << (8 * i) }.reduce(_ | _)
+        )
+        dut.io.cmd_payload.bits.rs2.poke(
+          Seq.range(1, -1, -1).map { i => (rs2_arr(i).toInt & 0xff) << (8 * i) }.reduce(_ | _)
+        )
+        dut.io.cmd_payload.ready.expect(true.B)
+        dut.io.rsp_payload.ready.poke(true.B)
+        dut.io.rsp_payload.valid.expect(true.B)
+        dut.io.rsp_payload.bits.rd.expect(
+          Seq.range(3, -1, -1).map { i => (rd_arr(i).toInt & 0xff) << (8 * i) }.reduce(_ | _)
+        )
+        dut.clock.step(1)
+      }
+    }
+  }
+}
