eceda84f19cf2272bd001c09b05e382e13cd3b39
yenpo <nn6124022@gs.ncku.edu.tw>
2024-01-11 01:01:07 +0800

    Update lab index in includes/instruction/

--- src/acal_lab/includes/instruction/SimdFuse.h
+++ src/acal_lab/includes/instruction/SimdFuse.h
@@ -8,10 +8,10 @@
 namespace acal_lab
 {
 
-// 8-2.1-3.3 : Signed Integer Multiplication : sPMULI8I16S_vv
+// 13-2.1-3.3 : Signed Integer Multiplication : sPMULI8I16S_vv
 void sPMULI8I16S_vv(int16_t c[4], int8_t a[4], int8_t b[4]);
 
-// 8-2.1-3.6 : Signed Integer Precise(Lossless) Multiplication : sPMULI8I16S_vx
+// 13-2.1-3.6 : Signed Integer Precise(Lossless) Multiplication : sPMULI8I16S_vx
 void sPMULI8I16S_vx(int16_t c[4], int8_t a[4], int8_t b);
 
 } // namespace acal_lab
--- src/acal_lab/libs/instruction/SimdFuse.cc
+++ src/acal_lab/libs/instruction/SimdFuse.cc
@@ -1,13 +1,13 @@
 #include "acal_lab/includes/instruction/SimdFuse.h"
 
-// 8-2.1-3.3 : Signed Integer Multiplication : sPMULI8I16S_vv
+// 13-2.1-3.3 : Signed Integer Multiplication : sPMULI8I16S_vv
 inline void acal_lab::sPMULI8I16S_vv(int16_t c[4], int8_t a[4], int8_t b[4])
 {
     sPMULI8I16S_vv_L(c, a, b);
     sPMULI8I16S_vv_H(c + 2, a, b);
 }
 
-// 8-2.1-3.6 : Signed Integer Precise(Lossless) Multiplication : sPMULI8I16S_vx
+// 13-2.1-3.6 : Signed Integer Precise(Lossless) Multiplication : sPMULI8I16S_vx
 inline void acal_lab::sPMULI8I16S_vx(int16_t c[4], int8_t a[4], int8_t b)
 {
     sPMULI8I16S_vx_L(c, a, b);
