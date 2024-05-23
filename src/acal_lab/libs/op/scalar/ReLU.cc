ebc01ab10d539d41655e74db04caaa320645f9f7
yenpo <nn6124022@gs.ncku.edu.tw>
2024-05-23 18:49:08 +0800

    Update ReLU.cc

--- src/acal_lab/libs/op/scalar/ReLU.cc
+++ src/acal_lab/libs/op/scalar/ReLU.cc
@@ -7,7 +7,7 @@ void ReLU::exec() {
 	for (int c = 0; c < input->C; c++) {
 		c_idx = c * input->H;
 		for (int h = 0; h < input->H; h++) {
-			h_idx = c_idx + h * input->W;
+			h_idx = (c_idx + h) * input->W;
 			for (int w = 0; w < input->W; w++) {
 				w_idx               = h_idx + w;
 				output->data[w_idx] = input->data[w_idx] > 0 ? input->data[w_idx] : 0;
